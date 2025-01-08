5165e3ffd51524ffef3f4475f0c0551e2646b59c
stanley-yin <stanley6140992@gmail.com>
2025-01-08 21:34:03 +0800

    feat: create ImageCard component

---
+++ src/app/components/ImageCard.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Image from "next/image";
+
+type ImageCardProps = {
+  img: string;
+  imgAlt: string;
+  title: string;
+  description: string;
+  createdAt: string;
+};
+const ImageCard = ({
+  img,
+  imgAlt,
+  title,
+  description,
+  createdAt,
+}: ImageCardProps) => {
+  return (
+    <div className="group relative max-w-lg">
+      <div className="relative h-64 w-full overflow-hidden rounded-lg">
+        <Image
+          src={img}
+          alt={imgAlt}
+          fill={true}
+          objectFit={"cover"}
+          className="default-transition hover:scale-105"
+        />
+      </div>
+      <p className="heading-2">{title}</p>
+      <p className="body-text-small text-gray-500">{createdAt}</p>
+      <p className="body-text line-clamp-3 hover:underline">{description}</p>
+    </div>
+  );
+};
+
+export default ImageCard;
