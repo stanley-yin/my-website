45f48ebf1dd157e81ca34a6fb78d3fd852b428c9
stanley-yin <stanley6140992@gmail.com>
2025-01-10 23:39:06 +0800

    feat: resume image add shadow

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -11,14 +11,13 @@ const Page = () => {
     <div className="wrapper py-6 sm:py-12">
       <CustomBreadcrumb crumbs={crumbs} />
       <h1 className="heading-1">Resume</h1>
-      <div className="relative mx-auto h-64 max-w-3xl">
-        <Image
-          src={"/images/Stanley_Resume.jpg"}
-          alt={"resume"}
-          width={800}
-          height={1000}
-        />
-      </div>
+      <Image
+        src={"/images/Stanley_Resume.jpg"}
+        alt={"resume"}
+        width={800}
+        height={1000}
+        className="mx-auto shadow-xl"
+      />
     </div>
   );
 };
